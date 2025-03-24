703ef6b1c4d684f6b7f7289d135c791568934f4a
kellbot <kellbot@gmail.com>
2025-03-23 22:50:14 -0400

    Fixed #2 and #3

--- src/game/objects/Planet.ts
+++ src/game/objects/Planet.ts
@@ -2,16 +2,19 @@ import { playerManager } from "../managers/PlayerManager";
 export class Planet extends Phaser.GameObjects.Container
 {
 
+    scene: Phaser.Scene;
     planetSprite: Phaser.GameObjects.Image;
     fx: any;
     minText: Phaser.GameObjects.Text;
 
+    useable: boolean = false;
     minCoinsNeeded: number;
 
     constructor(scene: Phaser.Scene, x: number, y: number)
     {
         super(scene, x, y);
 
+        this.scene = scene;
         this.minCoinsNeeded = 5;
 
         // Containers inherently do not have a size
@@ -24,29 +27,44 @@ export class Planet extends Phaser.GameObjects.Container
         this.add(this.minText);
 
         if (this.planetSprite) this.fx = this.planetSprite.preFX?.addColorMatrix();
-        if (this.fx) this.fx.grayscale(1);
+
 
         // Collision physics
         scene.physics.world.enable(this);
         if (this.body && this.body instanceof Phaser.Physics.Arcade.Body) {
             this.body.setCircle(24);
         }
+        this.setUseable(this.useable);
+    }
 
+    setUseable(value: boolean)
+    {
+        this.useable = value;
+        if (value) {
+            this.scene.physics.world.enable(this);
+            this.minText.setText('');
+            if (this.fx) this.fx.grayscale(0);
+        } else {
+            this.scene.physics.world.disable(this);
+            if (this.fx) this.fx.grayscale(1);
+        }
     }
 
     update(time: number, delta: number): void {
         if (playerManager.coinCount < this.minCoinsNeeded){
             this.minText.setText(`$${this.minCoinsNeeded} needed`);
         } else {
-            this.minText.setText('');
-            this.fx.grayscale(0);
+            this.setUseable(true);
         }
 
-         if (this.y < this.scene.cameras.main.height + 32)
-        {
-            this.y += 10 * delta / 1000;
-        } else {
-            this.destroy();
+        if (this.visible){
+            if (this.y < this.scene.cameras.main.height + 32)
+            {
+
+                this.y += 10 * delta / 1000;
+            } else {
+                this.destroy();
+            }
         }
 
     }
